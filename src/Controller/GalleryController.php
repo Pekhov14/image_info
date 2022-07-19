0ad699291d652b11769827d1bf112ce595fb7368
pekhov <pehovanton21@gmail.com>
2022-07-19 03:39:58 +0300

    Added sending form

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GalleryController extends AbstractController
 {
-    #[Route('/gallery', name: 'app_gallery')]
-    public function index(): Response
+    #[Route('/gallery/{slug}', name: 'app_gallery', methods: 'GET')]
+    public function index(string $slug): Response
     {
+        dd(base64_decode($slug));
+
         return $this->render('gallery/index.html.twig', [
             'controller_name' => 'GalleryController',
         ]);
--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Form\SearchImagesType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchController extends AbstractController
 {
     #[Route('/', name: 'app_search')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(SearchImagesType::class, null, [
+            'method' => 'GET'
+        ]);
+
+        $form->handleRequest($request);
+
+        $search = $form->get('search')->getData();
+
+        if ($search && $form->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('app_gallery', ['slug' => base64_encode($search)]);
+        }
+
         return $this->render('search/index.html.twig', [
-            'controller_name' => 'SearchController',
+            'form' => $form->createView()
         ]);
     }
 }
